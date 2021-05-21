6a35048417d26c0528946a157514b2414afa505f
qtdevops <qtdevops@gmail.com>
2021-05-21 17:43:59 +0530

    Added a simple square program

---
+++ May21/variables/square.py
@@ -0,0 +1,6 @@
+# This program reads the number provided from user and displays the square of 
+# the number
+number_from_input = int(input('Enter the number to find the square: '))
+square = number_from_input * number_from_input
+print(square)
+# waiting till 5: 41 PM
