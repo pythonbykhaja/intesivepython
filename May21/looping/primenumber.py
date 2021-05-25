2bfe115759728f82b7ec90828577d5734b85fef8
qtdevops <qtdevops@gmail.com>
2021-05-25 18:12:19 +0530

    Added code to use else statement with while

--- May21/looping/primenumber.py
+++ May21/looping/primenumber.py
@@ -2,17 +2,18 @@
 
 number = int(input('Enter the number: '))
 index = 2
-is_prime = True # Flag approch
 while index < number:
     if number % index == 0:
         # the number is divisible by index so not prime
         # should do some thing to specify number is not prime
-        is_prime = False
+        print(f"{number} is not prime")
         break
     index = index + 1
-
-if is_prime:
-    print(f"{number} is prime")
 else:
-    print(f"{number} is not prime")
+    # This code will be executed when the loop ended normally (no break call)
+    print(f"{number} is prime")
+
+print("Thanks for using my program")
+
+
 
