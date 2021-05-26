5a97f141b8243441f5ce6ee2e6f9a37192a8fc6a
qtdevops <qtdevops@gmail.com>
2021-05-26 17:49:44 +0530

    Added tuple demo

---
+++ May21/tuples/tupleiterations.py
@@ -0,0 +1,15 @@
+rainbow = ('Violet', 'Indigo', 'Blue', 'Green', 'Yellow', 'Orange', 'Red',)
+index = 0
+# Iterating using while
+print('Iteration using while')
+while index < len(rainbow):
+    print(rainbow[index])
+    index += 1
+
+# Iterating using for in
+print('Iteration using for in')
+for color in rainbow:
+    print(color)
+
+
+
