38dcb3f0937f24dda87210b1210620888ed7b66d
qtdevops <qtdevops@gmail.com>
2021-05-28 17:58:04 +0530

    Added solution 1

--- May21/lists/shoppinglist.py
+++ May21/lists/shoppinglist.py
@@ -12,4 +12,19 @@ while True:
     if choice == 'n':
         break
 
-print(items,items_count, price)
+print("Quality Fresh SuperMarket")
+print()
+print()
+print("item \t count \t unitprice \t total price")
+bill_amount = 0
+for index in range(len(items)):
+    item = items[index]
+    count = items_count[index]
+    price = price_per_item[index]
+    total_price = count * price
+    print(f"{item} \t {count} \t {price} \t \t {total_price}")
+    bill_amount += total_price
+
+print()
+print()
+print(f"Bill amount: {bill_amount}")
