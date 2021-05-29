f4c6115f705b18594c7d35054db284301db31134
qtdevops <qtdevops@gmail.com>
2021-05-29 16:29:17 +0530

    Added zip function to solve the shopping example

--- May21/lists/shoppinglist.py
+++ May21/lists/shoppinglist.py
@@ -1,11 +1,17 @@
-items = []
+products = []
+counts = []
+prices = []
+item_prices = []
 while True:
     item = input('Enter the item to be purchased: ')
     count = int(input('Enter the number of items to be purchased: '))
     price = float(input('Enter the price per unit: '))
     item_price = count * price
-    current_item_list = [item, count, price, item_price ]
-    items.append(current_item_list)
+    products.append(item)
+    counts.append(count)
+    prices.append(price)
+    item_prices.append(item_price)
+    
     choice = input('Do you want to continue press y to continue and n to exit ')
     if choice == 'n':
         break
@@ -15,11 +21,7 @@ print()
 print()
 print("item \t count \t unitprice \t total price")
 bill_amount = 0
-for current_item in items:
-    item = current_item[0]
-    count = current_item[1]
-    price = current_item[2]
-    total_price = current_item[3]
+for item, count, price, total_price in zip(products,counts,prices, item_prices):
     print(f"{item} \t {count} \t {price} \t \t {total_price}")
     bill_amount += total_price
 
