92bb9053ced650194436e488f27081f57ba7148c
qtdevops <qtdevops@gmail.com>
2021-06-03 17:46:05 +0530

    Added some more examples

--- May21/functions/basics.py
+++ May21/functions/basics.py
@@ -10,7 +10,37 @@ def say_hello():
     """
     print("hello")
 
+def pi():
+    """
+    This function returns the pi value
+    """
+    return 3.14
+
+def square(number):
+    """
+    This function returns the square of number
+
+    paramters:
+    number: 
+
+    returns:
+    square of number
+    """
+    return number ** 2
+
+def power(number, index):
+    """
+    This function can calculate the power 
+    """
+    return number ** index
+
+# calling function
 say_hello()
 do_nothing()
 say_hello()
+# Getting a return value from function
+pi_value = pi()
+print(pi_value)
+print(square(10))
+print(power(10,4))
 
