1a6321db4ca83e9d3992f6f6bce6fa60fae50067
qtdevops <qtdevops@gmail.com>
2021-06-03 18:27:04 +0530

    Added solution for 10001 prime number

--- May21/functions/primenumbers.py
+++ May21/functions/primenumbers.py
@@ -4,14 +4,17 @@ def is_prime(number):
 
     returns True if prime false otherwise
     """
-    
     for index in range(2, number//2):
         if number%index == 0:
             return False
     return True
-    
-min = 100
-max = 1000
-for number in range(min,max+1):
+
+prime_count = 0
+number = 1
+while (prime_count < 10001):
+    number += 1
     if is_prime(number):
-        print(number)
+        prime_count +=1
+ 
+# Print answer
+print(number)
