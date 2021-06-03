aca28eb59035c8bdfb25e0f5cec0e5df0745d639
qtdevops <qtdevops@gmail.com>
2021-06-03 18:03:06 +0530

    Added function for prime numbers

--- May21/functions/primenumbers.py
+++ May21/functions/primenumbers.py
@@ -1,9 +1,17 @@
+def is_prime(number):
+    """
+    This function will check if the number is prime or not
+
+    returns True if prime false otherwise
+    """
+    for index in range(2, number//2):
+        if number%index == 0:
+            return False
+    else:
+        return True
+
 min = 100
 max = 1000
 for number in range(min,max+1):
-    # if number is prime
-    for index in range(2,number//2):
-        if number%index == 0:
-            break
-    else:
+    if is_prime(number):
         print(number)
