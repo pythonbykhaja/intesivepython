8d8870ba7adaf2d8b9e07c3c643a17995a588c6e
qtdevops <qtdevops@gmail.com>
2021-06-03 17:34:00 +0530

    Added basic functions

---
+++ May21/functions/basics.py
@@ -0,0 +1,16 @@
+def do_nothing():
+    """
+    This function does nothing
+    """
+    pass
+
+def say_hello():
+    """
+    This function will say hello
+    """
+    print("hello")
+
+say_hello()
+do_nothing()
+say_hello()
+
--- May21/problems/main.py
+++ May21/problems/main.py
@@ -9,8 +9,19 @@ records = list(review_file_reader)
 count = len(records)
 print(f"number of records are {count}")
 
-# print the number of reviews per rating
-# 2818 => 100
-# 20168 => 
+users = [ record[3] for record in records ]
+unique_users = set(users)
+print(f"{len(unique_users)} have reviewed, users are {unique_users}")
 
+listings = [ record[0] for record in records ]
+unique_listings = set(listings)
+print(f"number of listings are {len(unique_listings)} , listings are {unique_listings}")
 
+listing_review = {}
+for listing in listings:
+    if listing in listing_review:
+        listing_review[listing] += 1
+    else:
+        listing_review[listing] = 1
+
+print(listing_review)
