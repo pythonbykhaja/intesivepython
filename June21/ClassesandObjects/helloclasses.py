77765d11c31b4c8c896ef23ad860b33aa04b1665
qtdevops <qtdevops@gmail.com>
2021-06-09 17:41:58 +0530

    Added sample printer class

---
+++ June21/ClassesandObjects/helloclasses.py
@@ -0,0 +1,25 @@
+class Printer:
+    '''
+    This class represents a printer
+    '''
+    def print(text):
+        """
+        This method represents the print functionality
+        """
+        pass
+
+    def switch_on():
+        """
+        This method switches on the printer
+        """
+        pass
+
+    def switch_off():
+        """
+        This method switches off the printer
+        """
+        pass
+
+# creating object
+hp_printer = Printer()
+print(type(hp_printer))
