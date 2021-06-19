83588a924d11b7e9b37098a876c39c10a550578d
qtdevops <qtdevops@gmail.com>
2021-06-19 17:07:00 +0530

    Added instance attributes

--- June21/ClassesandObjects/attributes.py
+++ June21/ClassesandObjects/attributes.py
@@ -1,3 +1,22 @@
 class Car:
+    """
+    This class represents an Car
+    """
+    def __init__(self, brand, model, reg_no):
+        """
+        Initalizing the Car with arguments
+        brand
+        model
+        reg_no
+        """
+        # instance attribute
+        self.brand = brand
+        self.model = model
+        self.reg_no = reg_no
+
     # class level attribute
     count = 0
+
+# car = Car(brand='Honda', model='City', reg_no='0353')
+# print(car.brand)
+# print(car.model)
--- June21/ClassesandObjects/helloclasses.py
+++ June21/ClassesandObjects/helloclasses.py
@@ -23,3 +23,45 @@ class Printer:
 # creating object
 hp_printer = Printer()
 print(type(hp_printer))
+
+
+class Car:
+    """
+    This class represents a car
+    """
+    def start():
+        """
+        This method is used for starting the car
+        """
+        pass
+
+    def stop():
+        """
+        This method is used for stopping the Car
+        """
+        pass
+
+    def drive():
+        """
+        This method is used for driving the car
+        """
+        pass
+
+
+class Laptop:
+    """
+    This class represents a Laptop
+    """
+    def power_on():
+        pass
+
+    def power_off():
+        pass
+
+    def start_program(program):
+        pass
+
+    def kill_program(program):
+        pass
+
+
