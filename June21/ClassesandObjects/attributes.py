e9a1cccdaf42fca85af9e4ae016367b3170b5578
qtdevops <qtdevops@gmail.com>
2021-06-19 17:24:51 +0530

    Added instance and class level attributes

--- June21/ClassesandObjects/attributes.py
+++ June21/ClassesandObjects/attributes.py
@@ -13,10 +13,24 @@ class Car:
         self.brand = brand
         self.model = model
         self.reg_no = reg_no
+        Car.count += 1
+
+    def info(self):
+        """
+        This instance method prints the information about the 
+        instance attributes and class attributes defined
+        """
+        print(f"Brand = {self.brand} id = {id(self.brand)}")
+        print(f"Model = {self.model} id = {id(self.model)}")
+        print(f"Reg-No = {self.reg_no} id = {id(self.reg_no)}")
+        print(f"Count = {Car.count}, id = {id(Car.count)}")
 
     # class level attribute
     count = 0
 
-# car = Car(brand='Honda', model='City', reg_no='0353')
-# print(car.brand)
-# print(car.model)
+ms_car = Car(brand='Maruti', model='Swift', reg_no='123')
+hc_car = Car(brand='Hyundai', model='Creta', reg_no='124')
+ms_car.info()
+hc_car.info()
+
+
