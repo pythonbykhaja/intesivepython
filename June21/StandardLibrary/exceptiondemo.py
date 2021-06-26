acec7d550e826abebeaa53f0c764e7b0316ed540
qtdevops <qtdevops@gmail.com>
2021-06-26 11:40:36 +0530

    Added exception handling

--- June21/StandardLibrary/exceptiondemo.py
+++ June21/StandardLibrary/exceptiondemo.py
@@ -2,16 +2,23 @@ items = ["Clothes", "phones", "laptops", "Chocolates"]
 
 if __name__ == "__main__":
     while True:
-        for index in range(0,len(items)):
-            print(f"{index}  ")
         try:
+            for index in range(0,len(items)):
+                print(f"{index}  ")
             option = int(input("Enter the number of your choice to get gift: "))
             print(f"You have choosen {items[option]}")
+        
+        except:
+            print("Wrong option entered try again")
+
+        else:
+            print("Thank god no errors")
+            
+        finally:
             choice = input('Do you want to Continue Enter y for yes and n for no: ')
             if choice == 'n':
                 break
-        except IndexError:
-            print("Wrong option selected try again")
+
 
 
 
