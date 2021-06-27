f7bbaa3e5f7113c4be0e46011b41f71444b6e08f
qtdevops <qtdevops@gmail.com>
2021-06-27 08:46:54 +0530

    Added writing to text file

--- June21/filesanddirectories/basicfileoperations.py
+++ June21/filesanddirectories/basicfileoperations.py
@@ -11,5 +11,25 @@ def write_to_file_using_print():
         print(index, file=print_file_obj)
     print_file_obj.close()
 
+
+def write_to_file_using_write():
+    """
+    This function is used to demonstrate using write funcion 
+    """
+    file_name = "review.txt"
+    file_obj = open(file=file_name, mode='wt')
+    quotes = '''The purpose of our lives is to be happy. — Dalai Lama,
+    Life is what happens when you’re busy making other plans. — John Lennon
+    Get busy living or get busy dying. — Stephen King
+    You only live once, but if you do it right, once is enough. — Mae West
+    '''
+    print(f'length of quotes: {len(quotes)}')
+    count = file_obj.write(quotes)
+    print(f"file written succesfully and the return value is {count}")
+    file_obj.close()
+    
+
+
 if __name__ == '__main__':
     write_to_file_using_print()
+    write_to_file_using_write()
