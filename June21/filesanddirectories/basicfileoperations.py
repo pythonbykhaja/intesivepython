ff514f3f5e56c0804479d923a15bec39eaa04a39
qtdevops <qtdevops@gmail.com>
2021-06-27 09:11:01 +0530

    Added code to read lines

--- June21/filesanddirectories/basicfileoperations.py
+++ June21/filesanddirectories/basicfileoperations.py
@@ -28,8 +28,76 @@ def write_to_file_using_write():
     print(f"file written succesfully and the return value is {count}")
     file_obj.close()
     
+def read_all_contents():
+    """
+    This function will demonstrate the read method
+    """
+    computer_file_obj = open('computer.txt', 'rt')
+    information = computer_file_obj.read()
+    print(information)
+    computer_file_obj.close()
+
+def read_by_chunks(chunk_size=100):
+    """
+    This function demonstrates the read by chunk size (number of characters)
+    """
+    computer_file_obj = open('computer.txt', 'rt')
+    fragment_count = 0
+    while True:
+        fragment = computer_file_obj.read(chunk_size)
+        if not fragment:
+            break
+        fragment_count += 1
+        print(f'Fragment {fragment_count} \n{fragment}')
+    computer_file_obj.close()
+
+def read_by_lines():
+    """
+    This method demonstrates readline
+    """
+    computer_file_obj = open('computer.txt', 'rt')
+    line_count = 0
+    while True:
+        line = computer_file_obj.readline()
+        if not line:
+            break
+        line_count += 1
+        print(f"line: {line_count}\n{line}")
+    computer_file_obj.close()
+
+def read_all_lines():
+    """
+    This method demonstrates readlines()
+    """
+    computer_file_obj = open('computer.txt', 'rt')
+    lines = computer_file_obj.readlines()
+    for line in lines:
+        print(line)
+
+
+
+
+def seperate_bw_prints(message=""):
+    for index in range(100):
+        print('#', sep='', end='')
+    print()
+    print(message)
+    for index in range(100):
+        print('#', sep='', end='')
+    print()
+
 
 
 if __name__ == '__main__':
-    write_to_file_using_print()
-    write_to_file_using_write()
+    #write_to_file_using_print()
+    #write_to_file_using_write()
+    seperate_bw_prints('Read all contents')
+    read_all_contents()
+    
+    seperate_bw_prints('Read by Chunk')
+    read_by_chunks(chunk_size=1000)
+    seperate_bw_prints('Read by lines')
+    read_by_lines()
+    seperate_bw_prints('Reading all lines')
+    read_all_lines()
+    
