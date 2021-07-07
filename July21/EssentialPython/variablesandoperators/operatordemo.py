119692beab92bd741a5009e0a7195e18dac39cc6
qtdevops <qtdevops@gmail.com>
2021-07-07 17:39:39 +0530

    Added basic operator demo

---
+++ July21/EssentialPython/variablesandoperators/operatordemo.py
@@ -0,0 +1,21 @@
+# Calculating area of rectangle
+length = 10
+width = 13
+area = length * width
+print('area of rectangle is ')
+print(area)
+
+# calculate area of circle
+pi = 3.14
+radius = 3
+area = pi * radius ** 2
+print('area of circle is ')
+print(area)
+
+# calculate simple interest
+prinicipal = 100000
+rate = 18
+time = 2
+si = prinicipal*rate*time/100
+print('Simple interest is')
+print(si)
