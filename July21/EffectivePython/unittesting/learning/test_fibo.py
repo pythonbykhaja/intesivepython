42e877f73cb0f0990b9cdc788e45e6f12dc07418
qtdevops <qtdevops@gmail.com>
2021-07-30 19:00:13 +0530

    Added pytest testcases

--- July21/EffectivePython/unittesting/learning/test_fibo.py
+++ July21/EffectivePython/unittesting/learning/test_fibo.py
@@ -1,38 +1,20 @@
-import unittest
-from unittest import result
 from fibo import fibonacci
 
-class TestFibonacci(unittest.TestCase):
-    """This TestCase will test the fibonacci functionality
-
+def test_fibo_zero_index():
     """
+    This test case will test fibonnaci zero index
+    """
+    assert fibonacci(0) == 0
 
-    def test_fibo_zero_index(self):
-        """
-        This test step will test the zero index of fibonacci
-        """
-        result = fibonacci(0)
-        self.assertEqual(result , 0, "Result should be zero")
-        
-    def test_fibo_first_index(self):
-        """
-        This test step will test the first index of fibonacci
-        """
-        result = fibonacci(1)
-        self.assertEqual(result, 1, "Result should be one")
+def test_fibo_first_index():
+    """
+    This test case will test first index
+    """
+    assert fibonacci(1) == 1
 
-    def test_fibo_other_indexes(self):
-        """
-        This test step will test the fibonnaci with other indexes
-        """
-        result = fibonacci(2)
-        self.assertEqual(result, 1, "Result should be one")
-        result = fibonacci(3)
-        self.assertEqual(result, 2)
-        result = fibonacci(4)
-        self.assertEqual(result, 3)
-
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_fibo_other_indexes():
+    """
+    This will test other indexes
+    """
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
