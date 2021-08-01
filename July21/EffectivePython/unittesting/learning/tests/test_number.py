1a9cd78464666b9f9456401e5b9338774713a316
qtdevops <qtdevops@gmail.com>
2021-08-01 17:47:31 +0530

    Added fix for exception message

--- July21/EffectivePython/unittesting/learning/tests/test_number.py
+++ July21/EffectivePython/unittesting/learning/tests/test_number.py
@@ -12,6 +12,6 @@ def test_iseven_positive():
 
 def test_iseven_negative():
     number_1 = Number(-2)
-    with pytest.raises(ValueError, match='Number cannot be less than zero'):
+    with pytest.raises(ValueError, match='Number should be greater than zero'):
         number_1.is_even()
 
