b8795149ed9cc7427bb3bba5f393bfc74c6c88e5
qtdevops <qtdevops@gmail.com>
2021-08-10 16:37:00 +0530

    Added keyword argument

--- July21/EssentialPython/workshop/functions/utils.py
+++ July21/EssentialPython/workshop/functions/utils.py
@@ -10,3 +10,16 @@ def product(*args):
     for arg in args:
         result *= arg
     return result
+
+def convert_curreny(amount, **kwargs):
+    """This function converts from one currency to other
+    """
+    #amount = kwargs['amount']
+    source = kwargs['source']
+    destination = kwargs['destination']
+    rate = kwargs['rate']
+    target = amount * rate
+    return target
+
+if __name__ == '__main__':
+    convert_curreny(source='usd', destination='inr', amount=135, rate=74.5)
