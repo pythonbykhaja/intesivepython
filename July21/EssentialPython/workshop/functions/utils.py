7b0531d659a515fa5bfa9920b2465d755603e497
qtpython <qtkhajacloud@gmail.com>
2021-08-11 16:35:16 +0530

    Added variables for functions

--- July21/EssentialPython/workshop/functions/utils.py
+++ July21/EssentialPython/workshop/functions/utils.py
@@ -21,5 +21,22 @@ def convert_curreny(amount, **kwargs):
     target = amount * rate
     return target
 
+def is_leap(year):
+    """This function finds if the year passed is leap or not
+    Arguments:
+      year (int): year 
+
+    Return: True if leap false other wise
+    """
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        return True
+    else:
+        return False
+
 if __name__ == '__main__':
     convert_curreny(source='usd', destination='inr', amount=135, rate=74.5)
