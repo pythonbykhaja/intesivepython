64e4cf734fb6fce7878719a066e1c615995667dd
qtpython <qtkhajacloud@gmail.com>
2021-08-18 16:43:08 +0530

    Added class with init method

---
+++ July21/EssentialPython/workshop/classesandobjects/basics.py
@@ -0,0 +1,10 @@
+class Student:
+    """This class represents a Student
+
+    
+    """
+
+    def __init__(self, name, email) -> None:
+        self.name = name
+        self.email = email
+
---
+++ July21/EssentialPython/workshop/classesandobjects/usage.py
@@ -0,0 +1,4 @@
+from basics import Student
+
+if __name__ == '__main__':
+    s1 = Student('khaja', 'khaja.python@gmail.com')
