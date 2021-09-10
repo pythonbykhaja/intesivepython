407dc1d42200a870b71f057893256e5b716b4956
qtpython <qtkhajacloud@gmail.com>
2021-09-10 16:37:25 +0530

    Added support for yaml

--- Aug21/EffectivePython/Data/library/bookmodel.py
+++ Aug21/EffectivePython/Data/library/bookmodel.py
@@ -1,11 +1,11 @@
-import json
+import yaml
 import os
 
 def books_store_location():
     """
     This method will return the books location
     """
-    return "data/books.json"
+    return "data/books.yaml"
 
 def parse_books_into_dict():
     """
@@ -14,7 +14,7 @@ def parse_books_into_dict():
     if not os.path.exists(books_store_location()):
         return empty_books_dictionary()
     with open(books_store_location(), mode='r') as books_file:
-        return json.load(books_file)
+        return yaml.load(books_file)
 
 
 def empty_books_dictionary():
@@ -29,7 +29,7 @@ def save_books(books_dict):
     This method is used to save the books dictionary
     """
     with open(books_store_location(), mode='wt') as books_file:
-        json.dump(books_dict,books_file, indent=4)
+        yaml.dump(books_dict,books_file)
 
 def empty_book():
     """
