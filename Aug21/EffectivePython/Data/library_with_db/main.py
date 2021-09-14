248c37a738d687c01d0538ffbdbb7d74e7d17ff8
qtpython <qtkhajacloud@gmail.com>
2021-09-14 16:38:29 +0530

    Added code to delete the book, update the book

--- Aug21/EffectivePython/Data/library_with_db/main.py
+++ Aug21/EffectivePython/Data/library_with_db/main.py
@@ -2,5 +2,10 @@ import models
 
 if __name__ == '__main__':
     models.initialize_database()
-    #models.add_book('Who moved my cheese', 'spenser')
+    models.add_book('Who moved my cheese', 'spenser')
     models.search_book_by_title('Who')
+
+    for id in models.search_book_by_title('Who'):
+        models.update_book(id, 'Who Moved My Cheese', 'Dr Spenser Johnson')
+
+    models.delete_book_by_title('Who')
--- Aug21/EffectivePython/Data/library_with_db/models.py
+++ Aug21/EffectivePython/Data/library_with_db/models.py
@@ -48,10 +48,46 @@ def search_book_by_title(title):
         rows = cursor.fetchall()
         for row in rows:
             print(f"id --> {row[0]} title --> {row[1]} author --> {row[2]} ")
+            yield row[0]
     except Error as e:
         print(e)
     finally:
         connection.close()
+
+def update_book(id, new_title, new_author):
+    """
+    update the book with new_title and new_author
+    """
+    connection = None
+    try:
+        connection = sqlite3.connect(database_file_path())
+        sql = f""" UPDATE books
+        SET title='{new_title}', author='{new_author}'
+        where id = {id}
+        """
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()
+    except Error as e:
+        print(e)
+    finally:
+        connection.close()
+
+
+def delete_book_by_title(title):
+    """
+    Delete the book by title
+    """
+    for id in search_book_by_title(title):
+        try:
+            sql = 'DELETE FROM books where id=?'
+            connection = sqlite3.connect(database_file_path())
+            connection.execute(sql, (id,))
+            connection.commit()
+        except Error as e:
+            print(e)
+        finally:
+            connection.close()
 
 
 
