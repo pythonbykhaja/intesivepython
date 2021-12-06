b1e91de9a67d5fb4f96bf21b0bedcddc9b1d4712
qtdevops <qtdevops@gmail.com>
2021-12-06 08:39:31 +0530

    Added code with docstring

---
+++ Dec21/UnitTesting/sample1/calc.py
@@ -0,0 +1,12 @@
+def add(number1, number2):
+    """
+    This functions adds two numbers
+
+    Arguments:
+      number1 : first number to be passed
+      number2 : second number to be passed
+
+    Returns:
+      the result of two numbers
+    """
+    return number1 + number2
