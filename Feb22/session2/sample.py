9a678bc584d8777fc370c92017c66829a6f97fc0
qtkhajacloud <qtkhajacloud@gmail.com>
2022-02-21 19:42:18 +0530

    Added basic code for session 2

---
+++ Feb22/session2/sample.py
@@ -0,0 +1,14 @@
+def add(number1, number2):
+    """adding two numbers
+
+    Args:
+        number1: This is the first argument
+        number2: This is the second argument
+    Returns:
+        sum of two arguments
+    
+    """
+    return number1 + number2
+
+if __name__ == "__main__":
+    print(add(5,6))
